cd50cff2b9ee1eeec6cfc80737d73b37fadf1665
Nikita Volokhov <60272801+Volokhov-mda@users.noreply.github.com>
2022-03-08 01:55:31 +0300

    fix: start journey localization

--- src/components/ui/StartJourney/StartJourney.js
+++ src/components/ui/StartJourney/StartJourney.js
@@ -1,23 +1,40 @@
 import clsx from "clsx";
 import { useAtom } from "jotai";
 
-import { themeAtom } from "data/atoms";
+import { languageAtom, themeAtom } from "data/atoms";
+
+import localizedText from "consts/localizedText.json";
 
 import logo from "images/logos/logo.svg";
 
 import Icon from "components/ui/Icon";
 
 import styles from "./start-journey.module.css";
+import { useMemo } from "react";
 
 const StartJourney = ({ showButton, onClick, className, ...props }) => {
   const [theme] = useAtom(themeAtom);
+  const [language] = useAtom(languageAtom);
+
+  const startJourneyText = useMemo(() => {
+    return localizedText[language].startJourney.split(/(?=[ ])|(?<=[ ])/g);
+  }, [language]);
 
   className = clsx(styles.startJourney, styles[theme], className);
 
   return (
     <div className={className} {...props}>
-      <button onClick={onClick} className={clsx(styles.button, !showButton && styles.hidden)}>
-        Start <br /> Journey
+      <button
+        onClick={onClick}
+        className={clsx(styles.button, !showButton && styles.hidden)}
+      >
+        {startJourneyText.map((value, i) => {
+          if (value === " ") {
+            return <br />;
+          }
+
+          return value;
+        })}
       </button>
       <div className={styles.logoWrapper}>
         <Icon icon={logo} className={styles.logo} />
